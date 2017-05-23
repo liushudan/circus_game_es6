9aec873d442de8c054c36190ec5bffc0fe3df2fe
Paolo Dávila <pdavila@iesebre.com>
2017-05-23 19:53:17 +0200

    Delete code puuuuf

--- src/states/Game.js
+++ src/states/Game.js
@@ -6,25 +6,7 @@ export default class extends Phaser.State {
   init () {}
   preload () {}
 
-  create () {
-    const bannerText = 'Phaser + ES6 + Webpack'
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
-    banner.font = 'Bangers'
-    banner.padding.set(10, 16)
-    banner.fontSize = 40
-    banner.fill = '#77BFA3'
-    banner.smoothed = false
-    banner.anchor.setTo(0.5)
-
-    this.mushroom = new Mushroom({
-      game: this,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
-
-    this.game.add.existing(this.mushroom)
-  }
+  create () {}
 
   render () {
     if (__DEV__) {
